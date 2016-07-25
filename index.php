ecbc66c3279f9df1fd7f4161c11541f8f17a2440
sspangsberg <soren@spangsberg.net>
2016-07-25 12:36:39 +0200

    Add index.php and stylesheet.css

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<?php
+			echo("This is a test");
+		?>
+
+		<div id="red"></div>
+		<div id="green"></div>
+		<div id="blue"></div>
+		<div id="yellow"></div>
+	</body>
+</html>
